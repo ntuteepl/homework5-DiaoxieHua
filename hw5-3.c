86ebbc362285c754a743020c1bc8806c5946224b
DiaoxieHua <146065243+DiaoxieHua@users.noreply.github.com>
2023-10-31 15:52:51 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    double t;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if (a<=60){
+        t=a*b;
+    }
+    else if (a>=61&&a<=120){
+        t=a*(b*1.33);
+    }
+    else if(a>121){
+        t=a*(b*1.66);
+    }
+    printf("%.1f",t);
+}
 
